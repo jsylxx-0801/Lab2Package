44743e0911305b41d084fd9f03bcc5d4a9760c05
Xiao Jin <jsylxx_0801@163.com>
2023-11-01 15:59:08 -0400

    make a small change of the statistics

--- R/statistics.R
+++ R/statistics.R
@@ -2,14 +2,14 @@
 #' @importFrom dplyr pull
 #' @importFrom dplyr %>%
 #' @export
-statistics <- function(data, statistic = "mean") {
+statistics <- function(data, stat_type = "mean") {
   # Check if the input statistic is valid
-  if (!(statistic %in% c("mean", "median", "sd"))) {
+  if (!(stat_type %in% c("mean", "median", "sd"))) {
     stop("Invalid statistic. Please choose 'mean', 'median', or 'sd'.")
   }
   
   # Define the calculations for each statistic
-  calculations <- list(
+  calculate <- list(
     mean = function(x) mean(x, na.rm = TRUE),
     median = function(x) median(x, na.rm = TRUE),
     sd = function(x) sd(x, na.rm = TRUE)
@@ -18,9 +18,9 @@ statistics <- function(data, statistic = "mean") {
   # Calculate the specified statistic for Average Medicare Payments
   result <- data %>%
     summarise(
-      summary_value = calculations[[statistic]](`Average Medicare Payments`)
+      stat_value = calculate[[stat_type]](`Average Medicare Payments`)
     ) %>%
-    pull(summary_value)
+    pull(stat_value)
   
   return(result)
 }
