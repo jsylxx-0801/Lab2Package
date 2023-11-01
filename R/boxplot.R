550ae0b8856daa65989745c910b007166970986a
Xiao Jin <jsylxx_0801@163.com>
2023-11-01 13:38:37 -0400

    check whole functions

--- R/boxplot.R
+++ R/boxplot.R
@@ -1,27 +1,15 @@
-boxplot <- function(data, payment = "Average Medicare Payments") {
-  if (payment != "Average Medicare Payments" &
-        payment != "Average Total Payments" &
-        payment != "Average Covered Charges") {
+boxplot <- function(data, payment_type = "Average Medicare Payments") {
+  if (!payment_type %in% c("Average Medicare Payments", "Average Total Payments", "Average Covered Charges")) {
     stop("Invalid payment type")
   }
-
-
-# Boxplot
-
- NULL %>%
-   ggplot(aes(x = data[["DRG Definition"]],
-              y = data[[payment]])) +
-   geom_boxplot(fill = "lightblue",
-                color = "white") +
-   theme_minimal() +
-   theme(axis.text.x = element_text(angle = 90,
-                                    hjust = 1,
-                                    size = 6),
-         plot.title = element_text(hjust = 0.5,
-                                   size = 15)) +
-   labs(x = "DRG Code",
-        y = payment,
-        title = paste0("Boxplot of ",
-                       payment,
-                       " and DRG Code"))
+  
+  ggplot(data, aes_string(x = "DRG Definition", y = payment_type)) +
+    geom_boxplot(fill = "lightblue", color = "white") +
+    scale_x_discrete(labels = unique(substr(data$`DRG Definition`, 1, 3))) +
+    theme_classic() +
+    theme(axis.text.x = element_text(angle = 90, hjust = 1, size = 6)) +
+    labs(x = "DRG Code",
+         y = payment_type,
+         title = paste("Plot of", payment_type, "by DRG code"))
 }
+
--- R/statistics.R
+++ R/statistics.R
@@ -1,3 +1,5 @@
+#' @importFrom dplyr %>%
+#' @export
 statistics <- function(data, type) {
   if (type != 'mean' && type != 'median' && type != 'sd') {
     stop("Invalid type. Choose from 'mean', 'sd', or 'median'.")
@@ -6,7 +8,7 @@ statistics <- function(data, type) {
   info <- data %>%
     group_by(DRG.Definition) %>%
     summarise(
-      summary_value = get(stat_type)(Average.Medicare.Payments, na.rm = TRUE)
+      summary_value = get(type)(Average.Medicare.Payments, na.rm = TRUE)
     )
   
   return(info)
